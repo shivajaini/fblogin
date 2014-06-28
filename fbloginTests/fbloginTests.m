87790ad2e9d6b9797f761779c41e237fcc0c5e2e
shivajaini <shiva.jaini@gmail.com>
2014-06-28 10:19:27 -0700

    Initial Commit

---
+++ fbloginTests/fbloginTests.m
@@ -0,0 +1,34 @@
+//
+//  fbloginTests.m
+//  fbloginTests
+//
+//  Created by Shiva Jaini on 6/28/14.
+//  Copyright (c) 2014 Shiva Jaini. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fbloginTests : XCTestCase
+
+@end
+
+@implementation fbloginTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
